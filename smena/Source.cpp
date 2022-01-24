2c7655fa4467d287ce38bb31c17be9c260bd105e
DelsinRowe26 <ilya.gritchin20020909@gmail.com>
2022-01-24 11:30:47 +0700

    add

--- smena/Source.cpp
+++ smena/Source.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 int main() {
-    int n, m, c = 0, my_num, sm_num = 0, my_num2, sm_num2 = 0, b = 0, c = 0, d = 0, e = 0;
+    int n, m, c = 0, my_num, sm_num = 0, my_num_two, sm_num_two = 0, b = 0, f = 0, d = 0, e = 0, g = 0, h = 0, k = 0;
     cin >> n >> m;
     int a[30][30];
 
@@ -15,28 +15,33 @@ int main() {
         }
         cout << endl;
     }
-    cin >> my_num >> my_num2;
-    // вывод
+    cin >> my_num >> my_num_two;
+    while ((my_num > (m * n)) || (my_num_two > (m * n))) {
+        cout << "Vvedite novie chisla";
+        cin >> my_num >> my_num_two;
+    }
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < m; j++) {
             if (a[i][j] % 10 == a[i][j]) cout << "   ";
             else if (a[i][j] % 100 == a[i][j]) cout << "  ";
             else if (a[i][j] % 1000 == a[i][j]) cout << " ";
             if (my_num == a[i][j]) {
-                sm_num = a[i][j];
+                //sm_num = a[i][j];
                 b = i;
-                c = j;
+                f = j;
+                g++;
             }
-            else if (my_num2 == a[i][j]) {
-                sm_num2 = a[i][j];
+            else if (my_num_two == a[i][j]) {
+                //sm_num_two = a[i][j];
                 d = i;
                 e = j;
+                h++;
             }
-            if (i == b && j == c) {
-                a[i][j] = sm_num;
+            if (i == b && j == f && g >= 1 && my_num_two > 0 && my_num_two <= m * n) {
+                a[i][j] = my_num_two;
             }
-            if (i == d && j == e) {
-                a[i][j] = sm_num2;
+            if (i == d && j == e && h >=1 && my_num > 0 && my_num <= m * n) {
+                a[i][j] = my_num;
             }
             cout << a[i][j];
         }
